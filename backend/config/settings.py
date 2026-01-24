cfa7bafc318c801a22019d84c2135a76583dbfca
mooaz sayyed <sayyedmooaz@gmail.com>
2026-01-24 20:08:30 +0530

    Development Dockerfiles push

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -117,7 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # CORS settings
 CORS_ALLOWED_ORIGINS = [
