a2a558fcc285aefef9fe7f9faa2b578ee715f2f1
mooaz sayyed <sayyedmooaz@gmail.com>
2026-01-25 18:07:40 +0530

    Update Docker and Nginx configurations for pre-production and production environments

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from core.views import health
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
-    path('health/', health, name='root_health'),  # Root-level health check
 ]
