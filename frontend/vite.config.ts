0db7bd51513c19b1e75b602dc087378491f52362
mooaz sayyed <sayyedmooaz@gmail.com>
2026-01-25 22:46:42 +0530

    vite chagne

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,11 +10,28 @@ export default defineConfig({
     strictPort: true,
     hmr: {
       clientPort: 5173,
-    }
+    },
+    allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      'nextgensis.mooazsayyed.live',
+      'preprod.nextgensis.mooazsayyed.live',
+      '54.87.192.112'
+    ]
   },
   preview: {
     host: '0.0.0.0',
     port: 5173,
-    strictPort: true
+    strictPort: true,
+    allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      'nextgensis.mooazsayyed.live',
+      'preprod.nextgensis.mooazsayyed.live',
+      'staging.nextgensis.mooazsayyed.live',
+      'dev.nextgensis.mooazsayyed.live',
+      'local.nextgensis.mooazsayyed.live',
+      '54.87.192.112'
+    ]
   }
 })
